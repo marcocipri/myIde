639dec798dc8a8d7618aa8c645a6a6a73388c174
marco cipri <marcocipri@gmail.com>
2019-08-23 21:09:13 +0200

    Add files via upload

---
+++ golang/install.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/marcocipri/dotfiles.git
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim  +PlugInstall +qall
