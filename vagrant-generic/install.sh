37115f747457d34cd1dbae7bf0da806d19f0d8a3
marco cipri <marcocipri@gmail.com>
2019-08-23 19:50:26 +0200

    Update install.sh

--- vagrant-generic/install.sh
+++ vagrant-generic/install.sh
@@ -1,17 +1,17 @@
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo add-apt-repository -y ppa:jonathonf/vim
-sudo apt-get -y update
-sudo apt-get -y install vim
-sudo  add-apt-repository  -y ppa:neovim-ppa/stable
-sudo apt-get  -y update
-sudo apt-get  -y install neovim
-sudo apt install -y zsh
-sudo apt-get install powerline fonts-powerline
+apt-get update
+apt-get -y upgrade
+add-apt-repository -y ppa:jonathonf/vim
+apt-get -y update
+apt-get -y install vim
+add-apt-repository  -y ppa:neovim-ppa/stable
+apt-get  -y update
+apt-get  -y install neovim
+apt install -y zsh
+apt-get install powerline fonts-powerline
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get install -y nodejs
-sudo apt-get update && sudo apt-get install yarn
+apt-get install -y nodejs
+apt-get update && sudo apt-get install yarn
 wget https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
-sudo tar -xvf go1.12.9.linux-amd64.tar.gz
-sudo mv go /usr/local
+tar -xvf go1.12.9.linux-amd64.tar.gz
+mv go /usr/local
