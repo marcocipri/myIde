4e91d794be697830693ab9b7fc39fae3554bf2e1
marco cipri <marcocipri@gmail.com>
2019-08-24 00:17:09 +0200

    Update install.sh

--- golang/install.sh
+++ golang/install.sh
@@ -3,4 +3,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
 vim +'PlugInstall --sync' +qall &> /dev/null
